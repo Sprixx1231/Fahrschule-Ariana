e9758939064182c3c4276c74a4392f0e384cc8c2
A E S T H E T I C <62722161+Sprixx1231@users.noreply.github.com>
2022-01-09 15:11:51 +0100

    Navbar mit Funktionierenden weiterleitungen implementiert. Fehlend: css bei den weiterleitungen

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Start from './pages/start.js';
+import Agbs from './pages/agbs.js'
+import BegleitetesFahren from './pages/begleitetes-fahren.js'
+import FuehrerscheinKlassen from './pages/fuehrerscheinklassen.js'
+import Impressum from "./pages/impressum";
+import Pruefung from "./pages/pruefung";
+import PraktischerUnterricht from "./pages/praktischer-unterricht";
+import TheoretischerUnterricht from "./pages/theoretischer-unterricht";
 
 
 function App() {
     return (
-        <>
             <Router>
+                    <Navbar />
                 <Routes>
-
+                    <Route path='/' element={<Start/>} />
+                    <Route path='/start' element={<Start/>} />
+                    <Route path='/fuehrerscheinklassen' element={<FuehrerscheinKlassen/>} />
+                    <Route path='/praktischer-unterricht' element={<PraktischerUnterricht/>} />
+                    <Route path='/theoretischer-unterricht' element={<TheoretischerUnterricht/>} />
+                    <Route path='/begleitetes-fahren' element={<BegleitetesFahren/>} />
+                    <Route path='/pruefung' element={<Pruefung/>} />
+                    <Route path='/impressum' element={<Impressum/>} />
+                    <Route path='/agbs' element={<Agbs/>} />
                 </Routes>
             </Router>
-        </>
     );
 }
 
---
+++ src/pages/pruefung.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Pruefung = () => {
+    return(
+        <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '90vh'
+        }}
+        >
+            <h1>Prüfungen</h1>
+        </div>
+    )
+}
+
+export default Pruefung;
